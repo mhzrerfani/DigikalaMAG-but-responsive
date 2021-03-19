17064c44c1e1506ee0081ffba18e858c50059a99
mhzrerfani <mhzrerfani@gmail.com>
2021-03-19 11:12:44 +0330

    Changed Cards Component

--- src/components/CardSlider/Cards.js
+++ src/components/CardSlider/Cards.js
@@ -2,6 +2,12 @@ import React, { Component } from "react";
 
 export default class Cards extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div className="flex-row">
+        <ul className="">
+          <li className="bg-blue-300 w-52 h-48"></li>
+        </ul>
+      </div>
+    );
   }
 }
